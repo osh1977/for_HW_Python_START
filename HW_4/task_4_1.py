56af250b0ae5f1b2804cf8d8d902c6b6c2dadaac
Oleg <osh1977@yandex.ru>
2023-05-15 22:01:14 +0300

    correction 4_1

--- HW_4/task_4_1.py
+++ HW_4/task_4_1.py
@@ -7,13 +7,7 @@
 # 3 6 9 12 15 18
 # 6 12'''
 
-# from random import randint
-# n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
-# print(n_set)
-# m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
-# print(m_set)
-# s_set = sorted(n_set.intersection(m_set))
-# print(*s_set)
+
 
 def create_array(n, mn, mx):
     import random
